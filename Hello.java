5080d66bd9239a81c7f146e4d3b4d26e2f624339
Tim-odm <t.makoni.tm16@gmail.com>
2022-12-05 19:11:43 +0000

    Modified Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		int i = 3;
+
+		for (int j = 0; j < i; j++) {
+			System.out.println(j);
+		}
 	}
 }
